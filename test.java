d8801020e89dc10d51ce8b65b942ac27bc3edbdc
erickim0223 <erickim0223@yahoo.com>
2023-11-13 20:40:29 -0500

    committing program

---
+++ test.java
@@ -0,0 +1,9 @@
+class myProgram {
+    public static void main(String args[]) {
+        hello("Eric");
+    }
+
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+}
