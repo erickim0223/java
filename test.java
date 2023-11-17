eb77b5e24d9249456f0dad582bec98106fac01bd
erickim0223 <erickim0223@yahoo.com>
2023-11-16 20:00:46 -0500

    overloadding

--- test.java
+++ test.java
@@ -1,13 +1,25 @@
 class myProgram {
     public static void main(String args[]) {
         hello("Eric");
+        System.out.println(add(1, 2));
+        System.out.println(add(1, 2, 3));        
+
     }
 
     public static void hello(String name) {
         System.out.println("Hello, " + name);
+
     }
 
     public static String returnString(String name) {
         return name;
+    }
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int add(int x, int y, int z) {
+        return x + y + z;
     }
 }
